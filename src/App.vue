4f7e649d803d76a222b554642192761b5587dc6b
HasithaIroshanaDilshan <hidilshan70@gmail.com>
2020-07-16 16:27:19 +0530

    initial publish

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-<body
-  style="background-image: url('background.png'); background-repeat: no-repeat; background-size: cover; background-position: center center; background-opacity: 0.5;"
->
+<body :style="cssProps">
+  <!-- :style="{ backgroundImage: 'url(' + require('@/components/background.png') + ')' }" -->
+
   <div id="div1">
-    <NavBar />
-    <div id="app">
-      <transition name="slide-fade">
-        <router-view></router-view>
-      </transition>
-      <link
-        rel="stylesheet"
-        href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
-        integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
-        crossorigin="anonymous"
-      />
-    </div>
+	<NavBar />
+	<div id="app">
+		<transition name="slide-fade">
+		<router-view></router-view>
+		</transition>
+		<link
+			rel="stylesheet"
+			href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+			integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+			crossorigin="anonymous"
+		/>
+	</div>
   </div>
   <!-- <img class="backgroundImg" src="http://placekitten.com/1500/1000"> -->
 </body>
@@ -40,28 +40,31 @@ import NavBar from "@/components/NavBar";
 export default {
   name: "app",
   components: {
-    NavBar
-    // firstPage
+	NavBar
+	// firstPage
 
-    // HelloWorld,
-    // Login
+	// HelloWorld,
+	// Login
   },
   data() {
-    return {
-      showAbstract: true
-    };
+	return {
+		showAbstract: true,
+		cssProps: {
+			backgroundImage: `url(${require("@/assets/background.png")})`
+		}
+	};
   },
 
   created() {
-    // console.log(window.location.pathname)
-    if (localStorage.getItem("loggedIn") == "true") {
-      this.showAbstract = false;
-      if (window.location.pathname == "/") {
-        this.$router.push("/Home");
-      }
-    } else {
-      this.$router.push("/Intro");
-    }
+	// console.log(window.location.pathname)
+	if (localStorage.getItem("loggedIn") == "true") {
+		this.showAbstract = false;
+		if(window.location.pathname == "/") {
+			this.$router.push("/Home");
+		}
+	} else {
+		this.$router.push("/Intro");
+	}
   }
 };
 </script>
@@ -71,20 +74,20 @@ export default {
 <style>
 @import "./transitions.css";
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  color: #2c3e50;
-  padding-left: 5px;
-  margin-left: 10px;
-  padding-right: 5px;
-  margin-right: 10px;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	/* text-align: center; */
+	color: #2c3e50;
+	padding-left: 5px;
+	margin-left: 10px;
+	padding-right: 5px;
+	margin-right: 10px;
 }
 
 body,
 html {
-  width: 100%;
-  min-height: 100vh;
+	width: 100%;
+	min-height: 100vh;
 }
 </style>
